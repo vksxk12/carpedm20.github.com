17bb0992fac1394ff5548bd194a577d80568055c
carpedm20 <carpedm20@gmail.com>
2013-12-26 20:31:11 +0900

    english version 0.8

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,11 @@
+function lessAll() {
+  $(".show-less:lang(ko)").text("자세히 보기");
+  $(".show-less:lang(ko)").switchClass("show-less", "show-more", 400);
+  $(".show-less:lang(en)").text("Show more");
+  $(".show-less:lang(en)").switchClass("show-less", "show-more", 400);
+  $(".full-text").switchClass("full-text", "short-text", 400);
+}
+
 $(document).ready(function(){
    $(":lang(ko)").show()
 
@@ -8,14 +16,26 @@ $(document).ready(function(){
      scrollThreshold: 0.5,
    });
 
-   $(".show-more a").on("click", function() {
+   $(".show-more a:lang(en)").on("click", function() {
     var $link = $(this);
     var $content = $(this).parent().parent().find("div.project_description");
     var linkText = $link.text().toUpperCase();
 
 		switchClasses($content);
 
-    $link.text(getShowLinkText(linkText));
+    $link.text(getShowLinkTextEn(linkText));
+
+    return false;
+  });
+
+   $(".show-more a:lang(ko)").on("click", function() {
+    var $link = $(this);
+    var $content = $(this).parent().parent().find("div.project_description");
+    var linkText = $link.text().toUpperCase();
+
+		switchClasses($content);
+
+    $link.text(getShowLinkTextKo(linkText));
 
     return false;
   });
@@ -28,7 +48,19 @@ $(document).ready(function(){
     }
   }
 
-  function getShowLinkText(currentText){
+  function getShowLinkTextEn(currentText){
+    var newText = '';
+
+    if (currentText === "SHOW MORE") {
+        newText = "Show less";
+    } else {
+        newText = "Show more";
+    }
+
+    return newText;
+  }
+
+  function getShowLinkTextKo(currentText){
     var newText = '';
 
     if (currentText === "자세히 보기") {
